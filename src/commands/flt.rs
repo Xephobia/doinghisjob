929ffefe3f215247236b5a7847515182b9a7056b
Xephobia <nathanvalletmarseille@gmail.com>
2021-08-31 16:10:08 +0200

    change flt command

--- src/commands/flt.rs
+++ src/commands/flt.rs
@@ -1,4 +1,4 @@
-use num::{rational::Ratio, Float};
+use num::rational::Ratio;
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
     model::prelude::*,
@@ -8,21 +8,22 @@ use serenity::{
 #[command]
 pub async fn flt(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let f: f64 = args.parse()?;
-    let (m, e, s) = f.integer_decode();
-    let format_exp = e.to_string() + &format!("\n`{:b}`", e); // the exponent is only 11 bits
-    let format_mant = m.to_string() + &format!("\n`{:b}`", e); // the mantissa is only 52 bits
     msg.channel_id
         .send_message(&ctx.http, |m| {
             m.embed(|e| {
-                e.field("float", f, false)
-                    .field(
-                        "ratio",
-                        Ratio::from_float(f).map_or("None".to_string(), |r| r.to_string()),
-                        false,
-                    )
-                    .field("sign", s, false)
-                    .field("exponent", format_exp, false)
-                    .field("mantissa", format_mant, false)
+                let e = e.field("float", f, false).field(
+                    "ratio",
+                    Ratio::from_float(f).map_or("irrational".to_string(), |r| r.to_string()),
+                    false,
+                );
+                let category = match f.classify() {
+                    std::num::FpCategory::Nan => "NaN",
+                    std::num::FpCategory::Infinite => "infinite",
+                    std::num::FpCategory::Zero => "zero",
+                    std::num::FpCategory::Subnormal => "subnormal",
+                    std::num::FpCategory::Normal => "normal",
+                };
+                e.field("type", category, false)
             })
         })
         .await?;
